3669e0110ccbae092481b74f01b6d6fb7d5ef67a
김기현 <luke0985>
2026-02-10 17:07:17 +0900

    fix : SecurityConfig에 PATCH 추가

--- src/main/java/com/quicktax/demo/config/SecurityConfig.java
+++ src/main/java/com/quicktax/demo/config/SecurityConfig.java
@@ -102,7 +102,7 @@ public class SecurityConfig {
                 "https://api.quicktax.site"
         ));
 
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
